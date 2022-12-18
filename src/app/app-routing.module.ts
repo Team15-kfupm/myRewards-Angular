8550b81521ac93c626794d86f09f6e8649a31da9
Ali-jdk <alisayafi90@gmail.com>
2022-12-19 02:26:38 +0300

    login and add offers

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {MainComponent} from "./components/main/main.component";
+import {LoginPageComponent} from "./components/login-page/login-page.component";
+import {RegPageComponent} from "./components/reg-page/reg-page.component";
+import {OffersPageComponent} from "./components/offers-page/offers-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:MainComponent},
+  {path:'login',component:LoginPageComponent},
+  {path:'sign-up',component:RegPageComponent},
+  {path:'offers',component:OffersPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
