c01569d7792a3ebbf70135984d8443e55160595c
Ali-jdk <alisayafi90@gmail.com>
2023-01-16 07:15:13 +0300

    offers page refactoring

--- src/app/components/offer-card/offer-card.component.ts
+++ src/app/components/offer-card/offer-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-offer-card',
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OfferCardComponent implements OnInit {
 
+  @Input() title = '';
+  @Input() description = '';
+  @Input() image = '';
+  @Input() sDate = '';
+  @Input() eDate = '';
   constructor() { }
 
   ngOnInit(): void {
