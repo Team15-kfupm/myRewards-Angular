0c1ff0195cdd6e0e4f94778ff9ae4913c36e52ea
Ali-jdk <alisayafi90@gmail.com>
2023-02-08 02:30:02 +0300

    fix bugs

--- src/app/components/offer-form/offer-form.component.ts
+++ src/app/components/offer-form/offer-form.component.ts
@@ -1,10 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {OffersService} from "../../services/offers.service";
 import {Offer} from "../../models/offer";
-import firebase from "firebase/compat";
-import Timestamp = firebase.firestore.Timestamp;
 
 @Component({
   selector: 'app-offer-form',
@@ -13,46 +10,55 @@ import Timestamp = firebase.firestore.Timestamp;
 })
 export class OfferFormComponent implements OnInit {
 
-  edit=false;
+  edit = false;
 
-  id='';
-  title=''
-  image=''
-  description=''
-  startDate=''
-  validityPeriod=0
+  id = '';
+  title = ''
+  image = ''
+  description = ''
+  startDate = ''
+  validityPeriod = 0
 
-  offerObj:Offer ={
-    id:'',
-    title:'',
-    description:'',
-    image:'',
-    startDate:'',
-    validityPeriod:0
+  offerObj: Offer = {
+    id: '',
+    title: '',
+    description: '',
+    image: '',
+    startDate: '',
+    validityPeriod: 0
   }
-  constructor(private offersService:OffersService,
-             private dialogRef: MatDialogRef<OfferFormComponent>, @Inject(MAT_DIALOG_DATA) public data:any)
-  {}
+
+  constructor(private offersService: OffersService,
+              private dialogRef: MatDialogRef<OfferFormComponent>, @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
 
   ngOnInit(): void {
 
     this.edit = this.data != null;
+    if (this.edit) {
+      this.id = this.data.id;
+      this.title = this.data.title;
+      this.image = this.data.image;
+      this.description = this.data.description;
+      this.startDate = this.data.startDate;
+      this.validityPeriod = this.data.validityPeriod;
+    }
 
 
   }
 
 
-  resetAll(){
-    this.id='';
-    this.title='';
-    this.image='';
-    this.description='';
-    this.startDate='';
-    this.validityPeriod=0;
+  resetAll() {
+    this.id = '';
+    this.title = '';
+    this.image = '';
+    this.description = '';
+    this.startDate = '';
+    this.validityPeriod = 0;
   }
 
-  onSubmit(){
-    if (this.title == '' || this.description == '' || this.startDate == '' || this.validityPeriod == 0){
+  onSubmit() {
+    if (this.title == '' || this.description == '' || this.startDate == '' || this.validityPeriod == 0) {
       alert('Fill all the fields')
       return
     }
@@ -60,7 +66,7 @@ export class OfferFormComponent implements OnInit {
     this.offerObj.id = this.id;
     this.offerObj.title = this.title;
     this.offerObj.description = this.description;
-    this.offerObj.image = this.title+" Image";
+    this.offerObj.image = this.title + " Image";
     this.offerObj.startDate = this.startDate;
     this.offerObj.validityPeriod = this.validityPeriod;
 
@@ -71,7 +77,7 @@ export class OfferFormComponent implements OnInit {
   }
 
 
-  close(){
+  close() {
     this.resetAll();
     this.edit = false;
     this.dialogRef.close()
@@ -79,23 +85,18 @@ export class OfferFormComponent implements OnInit {
   }
 
 
-  onSave(){
+  onSave() {
 
-    console.log("New title "+this.data.title)
-    this.offersService.updateOffer(this.data.id,{
-      title:this.data.title,
-      description: this.data.description,
-      image:this.data.image,
-      startDate: this.data.startDate,
-      validityPeriod: this.data.validityPeriod
+    this.offersService.updateOffer(this.id, {
+      title: this.title,
+      description: this.description,
+      image: this.image,
+      startDate: this.startDate,
+      validityPeriod: this.validityPeriod
     })
     this.dialogRef.close()
 
   }
-
-
-
-
 
 
 }
