29e3df0e98f44323cd13727eccdb8b144745d0ef
Ali-jdk <alisayafi90@gmail.com>
2023-04-05 07:30:37 +0300

    validators for offer form

--- src/app/main/components/offer-form/offer-form.component.ts
+++ src/app/main/components/offer-form/offer-form.component.ts
@@ -3,6 +3,7 @@ import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 import {Offer} from "../../../models/offer";
 import {OffersService} from "../../../services/offers.service";
 import {DatePipe} from "@angular/common";
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-offer-form',
@@ -12,7 +13,17 @@ import {DatePipe} from "@angular/common";
 })
 export class OfferFormComponent implements OnInit {
 
+
   edit = false;
+  offerForm = this.fb.group({
+    title: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]],
+    description: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(500)]],
+    startDate: ['', Validators.required],
+    endDate: ['', Validators.required],
+    image: [Validators.required]
+
+  });
+
 
   id = '';
   title = ''
@@ -20,7 +31,7 @@ export class OfferFormComponent implements OnInit {
   description = ''
   startDate = ''
   endDate = ''
-
+  fileName: string = ''
 
   offerObj: Offer = {
     id: '',
@@ -30,10 +41,11 @@ export class OfferFormComponent implements OnInit {
     startDate: '',
     endDate: ''
   }
-
   today = new Date();
 
+
   constructor(
+    private fb: FormBuilder,
     private offersService: OffersService,
     private dialogRef: MatDialogRef<OfferFormComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any) {
@@ -66,16 +78,12 @@ export class OfferFormComponent implements OnInit {
   }
 
   onSubmit() {
-    if (this.title == '' || this.description == '' || this.startDate == '') {
-      alert('Fill all the fields')
-      return
-    }
 
     this.offerObj.id = this.id;
-    this.offerObj.title = this.title;
-    this.offerObj.description = this.description;
-    this.offerObj.startDate = this.startDate;
-    this.offerObj.endDate = this.endDate;
+    this.offerObj.title = this.offerForm.controls.title.value!.toString();
+    this.offerObj.description = this.offerForm.controls.description.value!.toString();
+    this.offerObj.startDate = this.offerForm.controls.startDate.value!.toString();
+    this.offerObj.endDate = this.offerForm.controls.endDate.value!.toString();
 
 
     this.offersService.addOffer(this.offerObj, this.image).then(r => console.log(r));
@@ -103,6 +111,80 @@ export class OfferFormComponent implements OnInit {
     if (fileList) {
       console.log("FileUpload -> files", fileList);
       this.image = fileList[0];
+      this.fileName = fileList[0].name;
     }
   }
+
+
+  getTitleErrorMessage(): string {
+    const titleControl = this.offerForm.controls.title;
+
+    if (titleControl.touched) {
+      if (titleControl.hasError('required')) {
+        return 'Title is required';
+      }
+      if (titleControl.hasError('minlength')) {
+        return 'Title must be at least 3 characters';
+      }
+      if (titleControl.hasError('maxlength')) {
+        return 'Title must not exceed 50 characters';
+      }
+    }
+    return '';
+
+  }
+
+
+  getDescriptionErrorMessage(): string {
+    const descriptionControl = this.offerForm.controls.description;
+
+    if (descriptionControl.touched) {
+      if (descriptionControl.hasError('required')) {
+        return 'Title is required';
+      }
+      if (descriptionControl.hasError('minlength')) {
+        return 'Description must be at least 10 characters';
+      }
+      if (descriptionControl.hasError('maxlength')) {
+        return 'Description must not exceed 500 characters';
+      }
+    }
+    return '';
+  }
+
+  getDatesErrorMessage(): string {
+    const startDateControl = this.offerForm.controls.startDate;
+    const endDateControl = this.offerForm.controls.endDate;
+
+    if (startDateControl.touched) {
+      if (startDateControl.hasError('required')) {
+        return 'Start date is required';
+      }
+    }
+    if (endDateControl.touched) {
+      if (startDateControl.hasError('required')) {
+        return 'End Date is required';
+      }
+    }
+    return '';
+  }
+
+
+  getPictureErrorMessage() {
+    const pictureControl = this.offerForm.controls.image;
+
+    if (pictureControl.touched || pictureControl.dirty) {
+      if (pictureControl.hasError('required')) {
+        return 'Picture is required';
+      }
+    }
+
+    return '';
+  }
+
+  isFormValid(): boolean {
+
+    return this.offerForm.valid
+  }
+
 }
